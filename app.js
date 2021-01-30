16bb44c65b729e55bf03373e54b253b5a0c7c444
JonathonLLopez <jonathonllopez@gmail.com>
2021-01-30 14:07:03 -0600

    port variable

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -9,6 +10,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use('/', routes);
 
-app.listen(process.env.PORT || 5000, (req, res) => {
-  console.log(`Listening on port ${process.env.PORT || 5000}`)
+app.listen(port, (req, res) => {
+  console.log(`Listening on port ${port}`)
 })
